5da28f8950a0f9f35304c27c6fb0b9e9d6776645
Rachit Agarwal <rachit.agarwal@mavs.uta.edu>
2025-02-03 22:41:41 -0800

    Build for GitHub Pages

---
+++ docs/_shared/chunk-HYPS5N5G.js
@@ -0,0 +1,5 @@
+var a={name:"Bhaavika Parekh",role:"Designer",disciplines:["Developer","Prototyper","Animator","Illustrator","Modder"],url:"https://hamishw.com",bluesky:"hamishw.com",figma:"@Hamish",github:"HamishMW",repo:"https://github.com/HamishMW/portfolio",ascii:`__  __  __
+\\ \\ \\ \\ \\\u2215
+ \\ \\\u2215\\ \\
+  \\\u2215  \\\u2215
+`};export{a};
